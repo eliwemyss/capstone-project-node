c4ae2c99e60ca1683d8b7c797d3f1e586edb2e0a
Eli Wemyss <eliwemyss@gmail.com>
2018-09-03 11:26:41 -0700

    added mock data

---
+++ public/app.js
@@ -0,0 +1,44 @@
+var MOCK_SCORE_DATA = {
+	"scores": [
+		{
+			"id": "111111",
+			"home": "Tennessee",
+			"away": "ETSU",
+			"home_score": "63",
+			"away_score": "7"
+		},
+		{
+			"id": "222222",
+			"home": "South Carolina",
+			"away": "Georgia",
+			"home_score": "34",
+			"away_score": "27"
+		},
+		{
+			"id": "333333",
+			"home": "Florida",
+			"away": "Kentucky",
+			"home_score": "41",
+			"away_score": "20"
+		}
+	]
+};
+
+function getScoreUpdates(callback) {
+	setTimeout(function() {callback(MOCK_SCORE_DATA)}, 1);
+}
+
+function displayScores(data) {
+	for (index in data.scores) {
+		$('body').append(
+			'<p>' + 'data.scores[index].text' + '</p>');
+	}
+}
+
+function getAndDisplayScores() {
+	getScoreUpdates(displayScores);
+}
+
+$(function() {
+	getAndDisplayScores();
+})
