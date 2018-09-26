92f3f0727a9c4b0d9622b3dda0168622336313b1
Eli Wemyss <eliwemyss@gmail.com>
2018-09-25 21:59:49 -0700

    added login page

---
+++ public/js/index.js
@@ -0,0 +1,65 @@
+
+function login() {
+    $('.login').submit((event) => {
+        event.preventDefault();
+        const user = username.value;
+        const pass = password.value;
+        const data = { username: `${user}`, password: `${pass}` };
+        $.ajax({
+            url: '/api/auth/login',
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            data: JSON.stringify(data),
+            success: (response) => {
+                sessionStorage.setItem('token', response.token);
+                location.href = '/main.html';
+            },
+
+            error: (err) => {
+                $('.alert').attr('aria-hidden', 'false').removeClass('hidden');
+            },
+
+        });
+    });
+}
+
+
+function selectLogin() {
+    $('.landing').on('click', 'button.SelectLogin', () => {
+        location.href = '/login.html';
+    });
+}
+
+
+function submitNew() {
+    $('.createAccount').submit((event) => {
+        event.preventDefault();
+        const user = newusername.value;
+        const pass = newpassword.value;
+        const name = name.value;
+        const data = {
+            username: `${user}`, password: `${pass}`, name: `${name}`,
+        };
+        $.ajax({
+            url: '/api/user',
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            data: JSON.stringify(data),
+            success: (response) => {
+                $('.createForm').addClass('hidden');
+                $('.successful').removeClass('hidden');
+            },
+
+            error: (err) => {
+                console.log(err);
+                $('.alert').attr('aria-hidden', 'false').removeClass('hidden');
+            },
+
+        });
+    });
+}
+
+
+$(login);
+$(submitNew);
+$(selectLogin);
